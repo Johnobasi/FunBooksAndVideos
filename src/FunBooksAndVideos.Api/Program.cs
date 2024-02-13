ec3fcfaa7aa940cdd8833248dabe82efcdacbb31
John Inyang <johni@atlashxm.com>
2024-02-13 12:01:20 +0000

    removed local db conn

--- src/FunBooksAndVideos.Api/Program.cs
+++ src/FunBooksAndVideos.Api/Program.cs
@@ -19,20 +19,6 @@ builder.Services
 
 var app = builder.Build();
 
-//if(args.Length == 1 && args[0]?.ToLower()=="seeddata")
-//    SeedData(app);
-
-// //Seed Data
-//void SeedData(IHost app)
-//{
-//    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
-
-//    using (var scope = scopedFactory!.CreateScope())
-//    {
-//        var service = scope.ServiceProvider.GetService<DataSeeder>();
-//        service?.SeedData();
-//    }
-//}
 app.UseMiddleware<ExceptionMiddleware>();
 if (app.Environment.IsDevelopment())
 {
